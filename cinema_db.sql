7f09de8e1cc527c04896b7127bec34990e8b5f52
abrahamponcea <83612691+abrahamponcea@users.noreply.github.com>
2022-12-13 15:05:37 -0600

    Data Base

--- cinema_db.sql
+++ cinema_db.sql
@@ -2,27 +2,27 @@ create database if not exists cinema_db;
 
 use cinema_db;
 
+CREATE TABLE IF NOT EXISTS sala(
+	id_sala INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    numeroSala INT UNSIGNED NOT NULL,
+    num_asientos INT UNSIGNED NOT NULL,
+    disponible BOOL
+);
+
 CREATE TABLE IF NOT EXISTS pelicula(
 	id_pelicula INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     id_salaProyeccion INT NOT NULL,
     nombre VARCHAR(50) NOT NULL,
     director VARCHAR(50) NOT NULL,
     duracion TIME NOT NULL,
-    fechaEstreno DATETIME NOT NULL,
+    fechaEstreno DATE NOT NULL,
 	sinopsis TINYTEXT NOT NULL,
-    horarios enum('11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00'),
+    horarios DATETIME,
     CONSTRAINT fk_pelicula_sala
 		FOREIGN KEY (id_salaProyeccion)
         REFERENCES sala(id_sala)
         ON UPDATE CASCADE
         ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS sala(
-	id_sala INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    numeroSala INT UNSIGNED NOT NULL,
-    fila enum('A','B','C','D','E','F','G','H','I','J','K','L') NOT NULL,
-    asiento INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS boleto(
@@ -40,3 +40,28 @@ CREATE TABLE IF NOT EXISTS boleto(
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
+
+INSERT INTO sala (numeroSala, num_asientos, disponible)
+	VALUES ('1', '40', TRUE),
+			('2', '50', FALSE),
+            ('3', '40', FALSE),
+            ('4', '30', FALSE),
+            ('5', '40', TRUE),
+            ('6', '40', TRUE);
+            
+INSERT INTO pelicula (id_salaProyeccion, nombre, director, duracion, fechaEstreno, sinopsis, horarios)
+	VALUES ('1', 'Avatar: El camino del agua', 'James Cameron', '03:12:00', '2022-12-15', 'Ambientada más de una década después de los sucesos que tuvieron lugar en la primera película', '2022-12-15 12:00:00'),
+			('1', 'Black Panther: Wakanda Forever', 'Ryan Coogler', '02:41:00', '2022-11-10', 'Secuela del muerto', '2022-12-17 14:00:00');
+            
+INSERT INTO boleto (id_numSala, id_nomPelicula)
+	VALUES ('1', '2'),
+			('1', '2'),
+            ('1', '2'),
+            ('1', '2');
+
+ /*           
+SELECT * FROM pelicula;
+
+
+DELETE FROM pelicula WHERE id_pelicula = 2;
+*/
